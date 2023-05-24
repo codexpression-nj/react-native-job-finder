5953a1c66fa7fac9dca6d0d17e635ec06d608a99
codexpression-nj <n.codexpression@gmail.com>
2023-05-24 23:40:50 +0200

    update

--- app/service/jobService.js
+++ app/service/jobService.js
@@ -1,43 +1,45 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-export default JobService;
+// export default JobService;
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-const options = {
-    method: "GET",
-    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    headers: {
-      "X-RapidAPI-Key": '',
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-    },
-    params: { ...query },
+    const options = {
+        method: "GET",
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            "X-RapidAPI-Key": '',
+            "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+        },
+        params: { ...query },
+    };
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refresh = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { data, isLoading, error, refresh }
 };
-const fetchData = async () => {
-    setIsLoading(true);
 
-    try {
-      const response = await axios.request(options);
-
-      setData(response.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setError(error);
-      console.log(error)
-    } finally {
-      setIsLoading(false);
-    }
-  };
-  useEffect(() =>{
-    fetchData();
-  },[]);
-  
-  const refresh = () => {
-    setIsLoading(true);
-    fetchData();
-  };
-
-  return {data,isLoading,error,refresh}
-}
+export default useFetch;
