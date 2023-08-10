3a0296ec1a78b614192827672d4392510dd9d4a3
codexpression-nj <n.codexpression@gmail.com>
2023-08-10 23:27:38 +0200

    update

--- app/quiz/quiz.jsx
+++ app/quiz/quiz.jsx
@@ -19,6 +19,48 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#2c3e50',
     },
+    questionCard: {
+        marginTop: 50,
+        marginVertical: 10,
+        padding: 40,
+        // borderTopRightRadius: 40,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        shadowColor: '#171717',
+        shadowOffset: { width: -6, height: 6 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        // height:'40%'
+    },
+    quizOptions: (isOptionsDisabled, option, correctOption, currentOptionSelected) => ({
+        // backgroundColor:s COLORS.backgroundColour,
+        borderRadius: 5,
+        padding: 5,
+        paddingHorizontal: 50,
+        marginVertical: 5,
+        height: 60,
+        alignItems:'center',
+        backgroundColor: isOptionsDisabled ?
+            option == correctOption
+                ? COLORS.green
+                : option == currentOptionSelected
+                    ? 'red'
+                    : 'grey'
+            : '#46736E',
+        // borderWidth: 1
+        justifyContent:'center'
+    }),
+    nextButton:{
+        paddingHorizontal: 5,
+        paddingVertical: 15,
+         width: '30%', 
+         borderRadius: 5,
+        //  backgroundColor: COLORS.white,
+         alignItems:'center',
+         marginTop:20
+    }
+
 });
 
 //make this component available to the app
