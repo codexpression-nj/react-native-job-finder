cd073a2f1aba1ad7f8eb57da134e3019794fb768
codexpression-nj <n.codexpression@gmail.com>
2023-08-24 22:39:42 +0200

    update

--- app/quiz/quizResults.jsx
+++ app/quiz/quizResults.jsx
@@ -7,7 +7,43 @@ import { COLORS } from '../constants/theme';
 const QuizResults = () => {
     return (
         <View style={styles.container}>
-            <Text>QuizResults</Text>
+            <View style={{
+                backgroundColor:'#46736E',
+                width: '90%',
+                borderRadius: 20,
+                padding: 20,
+                alignItems: 'center'
+            }}>
+                <Text style={{color:COLORS.white,fontSize: 30}}>Your Score</Text>
+
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    marginVertical: 30
+                }}>
+                    <Text style={{
+                        fontSize: 100,
+                        color: COLORS.white,
+                        fontWeight: 'bold'
+                    }}>{score}</Text>
+                    <Text style={{
+                        fontSize: 100, color: COLORS.white,
+                        fontWeight: 'bold'
+                    }}> / { allQuestions.length }</Text>
+                </View>
+                {/* Retry Quiz button */}
+                <TouchableOpacity
+                onPress={()=>{
+                    navigation.navigate('Welcome');}}
+                style={styles.btn}>
+                    <Text style={{
+                        textAlign: 'center', color: COLORS.secondary, fontSize: 20
+                    }}>Retry</Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     );
 };
